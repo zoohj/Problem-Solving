6fc92d24ff734a25801a64edc5a33782a56505d2
주현지 <128765995+zoohj@users.noreply.github.com>
2026-02-27 18:38:43 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,27 +1,22 @@
-import math 
+import math
 
 def solution(brown, yellow):
-    answer = []
+
+    # 1. 전체 격자 수 계산
     total = brown + yellow
-    # y(세로)는 3부터 전체 격자 수의 제곱근까지
-    for i in range(3,int(math.sqrt(total))+1):
-        if total % i == 0:
-            y = i
-            x = total // y
+    answer = []
+
+    # 2. 세로(y)는 최소 3부터 전체 넓이의 제곱근까지 탐색
+    # 가로(x) >= 세로(y) 조건을 만족하기 위함
+    for y in range(3, int(math.sqrt(total)) + 1):
+        # 3. total의 약수인 경우만 체크
+        if total % y == 0:
+            x = total // y  # 가로 길이 계산
+            
+            # 4. ⭐️ 수식 검증 
+            # brown = 2*(가로+세로-1)-2 = 2(가로+세로)-4
             if 2*(x+y)-4 == brown:
                 answer = [x, y]
                 break
-    return answer 
-
-'''
-가로(x),세로(y)
-brown(테두리) = (가로+세로-1)*2-2 = 2(가로+세로)-4
-yellow(중앙) = 가로*세로 - brown
-전체 격자 개수 = 가로 * 세로 = brwon+yellow
-
-brown = 2(x+y)-4
-yellow = x*y - brown = x*y - (2x+2y-4) = xy - 2x -2y+4
-
-answer.append(x,y)
-
-'''
+                
+    return answer
