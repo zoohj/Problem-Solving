5617f7010f4bf95240190e696addc5200ae87586
주현지 <128765995+zoohj@users.noreply.github.com>
2026-03-11 10:58:22 +0900

    [Gold V] Title: 토마토, Time: 1440 ms, Memory: 100488 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,5 +1,6 @@
 # https://www.acmicpc.net/problem/7576
 # 7576 토마토
+# 알고리즘: bfs
 
 import sys
 from collections import deque
@@ -7,10 +8,8 @@ from collections import deque
 input = sys.stdin.readline
 
 m, n = map(int, input().split())
-box = [[0] for _ in range(n)]
+box = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(n):
-    box[i] = list(map(int, input().split()))
 
 queue = deque()
 zero_cnt = 0
@@ -19,13 +18,13 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def dfs():
+def bfs():
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < len(box) and 0 <= ny < len(box[0]) and box[nx][ny] == 0:
+            if 0 <= nx < n and 0 <= ny < m and box[nx][ny] == 0:
                 box[nx][ny] = box[x][y] + 1
                 queue.append((nx, ny))
 
@@ -40,16 +39,15 @@ for i in range(n):
 if zero_cnt == 0:
     print(0)
 else:
-    dfs()
+    bfs()
     feasibility = True
     max_num = 0
 
     for i in range(n):
         for j in range(m):
+            max_num = max(max_num, box[i][j])
             if box[i][j] == 0:
                 feasibility = False
-            elif box[i][j] > max_num:
-                max_num = box[i][j]
     if not feasibility:
         print(-1)
     else:
